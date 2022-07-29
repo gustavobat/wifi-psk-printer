c3cd79730e8d0e5a5b4ce26001b468dec5cf9d7a
Gustavo Batistela <gustavoabatistela@gmail.com>
2022-07-29 12:03:29 -0300

    fix: first connection printing
    The first connection was not being printed due to the way I was iterating xargs arguments in the shell command.
    I fixed by using the -I (replace) flag instead

--- linux.sh
+++ linux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nmcli -g NAME connection show | xargs -d '\n' sh -c 'for arg do echo "$arg": $(nmcli -s -g 802-11-wireless-security.psk connection show "$arg"); done'
+nmcli -g NAME connection show | xargs -d '\n' -I {} sh -c 'echo "{}": $(nmcli -s -g 802-11-wireless-security.psk connection show "{}")'
